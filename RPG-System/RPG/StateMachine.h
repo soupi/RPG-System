1bd67740463d4975823f87dbe19eff872d8c043f
Gil Mizrahi <suppiral@yahoo.com>
2013-05-14 00:13:49 +0300

    stack became a vector
    so we will be able to render all states on the stack

--- RPG-System/RPG/StateMachine.h
+++ RPG-System/RPG/StateMachine.h
@@ -1,7 +1,9 @@
+// The state machine controls and changes the state of the game.
+
 #pragma once
 
 #include <SFML\Graphics.hpp>
-#include <stack>
+#include <vector>
 #include <map>
 #include <string>
 #include "State.h"
@@ -9,40 +11,57 @@
 #include "StateParams.h"
 #include "EmptyState.h"
 
+using std::map;
+using std::string;
+using std::vector;
 
 class Controller;
 
 class StateMachine 
 {
 public:
-	StateMachine()  { _state_stack.push(new EmptyState); }
+	// Constructor for the state machine is initiallized with an empty state.
+	StateMachine()  { _state_stack.push_back(new EmptyState); }
+	// The Destructor deletes the allocated pointers in the state machine
 	~StateMachine() 
 	{
 		while (!_state_stack.empty())
 			Pop();
-	//	while (!_states.empty())
+		for (map<string, State*>::iterator it = _states.begin(); it != _states.end(); ++it)
+			delete (*it).second;
 	}
-	void Render(sf::RenderWindow& window) {	_state_stack.top()->Render(window); }
-	bool Update(Controller& ctrl, float elapsedTime) { return _state_stack.top()->Update(ctrl, elapsedTime); }
+	// the render function renders the all states on the stack state
+	void Render(sf::RenderWindow& window) 
+	{
+		for (vector<State*>::iterator it = _state_stack.begin(); it != _state_stack.end(); ++it)
+			(*it)->Render(window); 
+	}
+	// the update function updates the current state
+	bool Update(Controller& ctrl, float elapsedTime) { return _state_stack.back()->Update(ctrl, elapsedTime); }
+	// stacks a state on top of the current state
 	void Stack(std::string state, StateParams* params = NULL) 
 	{ 
-		if (params)
-			_state_stack.push(_states[state]->Enter(params));
-		else _state_stack.push(_states[state]->Enter()); 
+		// enter a new state
+		if (params) 
+			_state_stack.push_back(_states[state]->Enter(params));
+		else _state_stack.push_back(_states[state]->Enter()); 
 	}
+	// pop the current state -> back to the previous state.
 	void Pop() { 
 		if (!_state_stack.empty())
 		{
-			if (_state_stack.top()->Exit())
-				delete _state_stack.top();
-			_state_stack.pop();
+			if (_state_stack.back()->Exit())
+				delete _state_stack.back();
+			_state_stack.pop_back();
 		}
 	}
+	// changes the current state
 	void Change(std::string state, StateParams* params = NULL) 
 	{
 		Pop();
 		Stack(state, params);
 	}
+	// adds a state to state machine so we will be able to use it in the future.
 	void Add(std::string desc, State* state)
 	{
 		_states[desc] = state;
@@ -50,6 +69,6 @@ public:
 
 
 private:
-	std::stack<State*> _state_stack;
-	std::map<std::string, State*> _states;
+	vector<State*> _state_stack;
+	map<string, State*> _states;
 };
